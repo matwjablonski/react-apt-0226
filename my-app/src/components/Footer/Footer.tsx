6ac01c699c8925f97928f9da30aaed59cf8cafa9
Mateusz Jabłoński <mail@mateuszjablonski.com>
2026-02-23 11:32:56 +0100

    portal example

--- my-app/src/components/Footer/Footer.tsx
+++ my-app/src/components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@mui/material';
 import { FooterContainer } from './Footer.styled';
+import { Modal } from '../Modal/Modal';
+import { useState } from 'react';
 
-const Footer = () => (
-    <FooterContainer>
-        <p>Copyright © 2024 | Created by: Mateusz</p>
-        <Button variant="text">Text</Button>
-    </FooterContainer>
-);
+const Footer = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    return (
+        <FooterContainer>
+            <p>Copyright © 2024 | Created by: Mateusz</p>
+            <Button variant="text" onClick={() => setIsModalVisible((prev) => !prev)}>Text</Button>
+            <Modal title="Mój modal po kliknięciu w przycisk" content='akcja wyszla ze stopki' isVisible={isModalVisible}/>
+        </FooterContainer>
+    );
+}
 
 export default Footer;
